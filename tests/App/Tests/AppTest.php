e5c3424b0909538f369c306694f10c2ae65ae426
Charlène Bennevault <c.bennevault@gmail.com>
2021-08-16 11:35:27 +0200

    Implement phpUnit and symfony fixtures

---
+++ tests/App/Tests/AppTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase {
+    
+    public function testTestAreWorking() {
+        $this->assertEquals(3, 1+1);
+    }
+}
