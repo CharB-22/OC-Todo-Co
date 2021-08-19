4eb5561012f05155210870adc520f8fe4eacb99b
Charlène Bennevault <c.bennevault@gmail.com>
2021-08-19 15:58:21 +0200

    Create tests for task CRU

--- tests/Controllers/TaskControllerTest.php
+++ tests/Controllers/TaskControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controllers;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -12,14 +13,39 @@ class TaskControllerTest extends WebTestCase
     {
         $client = static::createClient();
         $client->request('GET', '/tasks');
-        $this->assertResponseStatusCodeSame(Response::HTTP_OK); 
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        // Add what it should display
+        $this->assertSelectorExists('img', '.slide-image');
     }
 
-    public function testCreateTask()
+    public function testDisplayAllTasks()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/tasks');
+        
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('.thumbnail')->count()
+        );
+    }
+
+    public function testDisplayCreateTaskForm()
     {
         $client = static::createClient();
         $client->request('GET', '/tasks/create');
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertSelectorTextContains('h1', 'Créer une nouvelle tâche');
+    }
+
+    public function testDisplayEditTaskForm()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/tasks/13/edit');
         $this->assertResponseStatusCodeSame(Response::HTTP_OK); 
+        // Add what it should display
+        $this->assertSelectorTextContains('h1', 'Modifier cette tâche');
     }
 
 }
